0152ea95f317ce2b62d7eb00486b3ff368fcaa4b
Vinícius Evid <vinevid@gmail.com>
2026-04-24 09:40:24 -0300

    Fix JSX closing tag

--- src/screens/ChatVoiceScreen.tsx
+++ src/screens/ChatVoiceScreen.tsx
@@ -88,7 +88,7 @@ export default function ChatVoiceScreen({ route }: any) {
           </TouchableOpacity>
         </View>
       </KeyboardAvoidingView>
-    </SafeAreaView>
+    </View>
   );
 }
 
